c474728c625e4f875af8ddf6dfd67b9bc66d8f87
victoriaprisco <p.victoria@wustl.edu>
2023-08-02 21:36:07 -0400

    getting playlists

--- frontend/src/Components/Dashboard.js
+++ frontend/src/Components/Dashboard.js
@@ -6,30 +6,28 @@ async function getProfile (token) {
     });
     return await result.json();
 }
+async function getPlaylists(token){
+    const result = await fetch("https://api.spotify.com/v1/me/playlists?limit=50", {
+        method: "GET", headers: { Authorization: `Bearer ${token}`}
+    });
+    return await result.json();
+}
 const params = new URLSearchParams(window.location.search);
+const token = await getToken(params.get("code"));
 const profile = await (async ()=>{
-    const token = await getToken(params.get("code"));
     const profile = await getProfile(token.access_token);
     return profile;
 })();
-
+const playlists = await (async ()=>{
+    const playlists = await getPlaylists(token.access_token);
+    return playlists;
+})();
 
 const Dashboard = () => {
-    
-    // .then((token)=>{
-    //     if(!token.error){
-    //         return token;
-    //     }
-    // });
-    // console.log("token:", token);
-    // getProfile(token.access_token).then((result)=>{
-    //     console.log(result);
-    //     profile = result;
-    // })
-    console.log(profile);
+    console.log(playlists);
     return(
     <>
-        {profile ?<h1>{profile.display_name}</h1> :  <h1>no work</h1>}
+        {profile ? <h1>hey {profile.display_name}!</h1> :  <h1>no work</h1>}
     </>
     );
 }
