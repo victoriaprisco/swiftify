1b42d9094a99e3c32b6001f87bdecd63f95b36e2
victoriaprisco <p.victoria@wustl.edu>
2023-11-07 14:41:58 -0600

    mobile reactive styles

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -6,7 +6,7 @@ import Spotify from './Spotify.js'
 const Login = () => {
     return (<>
         <div id="container">
-            <h1 id="welcome" className="desc">welcome to swiftify!</h1>
+            <h1 id="welcome">welcome to swiftify!</h1>
             <span id="subheader"><h3 className="desc">replace all the stolen songs with taylor's versions on your playlists</h3></span>
             <div className="desc"><button id="login-button" onClick={completeAuth}>login to spotify </button> </div>
         </div>
